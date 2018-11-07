40978ce0adc8d51ee4b84d9279f59dbedae754d7
Morgan Hallgren <morganh@axis.com>
2018-11-07 15:13:02 +0100

    moved events to own file again and README

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 This is the repository for the code which accompanies my post on Event Sourcing in Go. The original post is [here](http://jen20.com/2015/02/08/event-sourcing-in-go.html).
 
-Remade by @hallgren
+Remade by [@hallgren](https://github.com/hallgren)
 
 To build and run:
 
-- `go build`
-- `./go-event-sourcing-sample`
+- `make`
+- `./goes`
 
+To clean generated code and the binary:
+
+- `make clean`
--- frequent_flier_account.go
+++ frequent_flier_account.go
@@ -14,35 +14,6 @@ type FrequentFlierAccount struct {
 	status        Status
 }
 
-// Status represents the Red, Silver or Gold tier level of a FrequentFlierAccount
-type Status int
-
-const (
-	StatusRed    Status = iota
-	StatusSilver Status = iota
-	StatusGold   Status = iota
-)
-
-// go:generate stringer -type=Status
-
-type FrequentFlierAccountCreated struct {
-	AccountId         string
-	OpeningMiles      int
-	OpeningTierPoints int
-}
-
-type StatusMatched struct {
-	NewStatus Status
-}
-
-type FlightTaken struct {
-	MilesAdded      int
-	TierPointsAdded int
-}
-
-type PromotedToGoldStatus struct {
-}
-
 // CreateFrequentFlierAccount constructor
 func CreateFrequentFlierAccount(id string) *FrequentFlierAccount {
 	self := &FrequentFlierAccount{}
