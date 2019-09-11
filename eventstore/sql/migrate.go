c72f9f368bc2b13eecd726b6d7644f761182887e
hallgren <morgan.hallgren@gmail.com>
2019-09-11 21:52:23 +0200

    added index to sql migration

--- eventstore/sql/migrate.go
+++ eventstore/sql/migrate.go
@@ -3,6 +3,8 @@ package sql
 func (sql *SQL) Migrate() error {
 	sqlStmt := `
 	create table events (id varchar not null, version integer, reason varchar, aggregate_type varchar, data varchar max, meta_data varchar max);
+	create unique index primary_key on events (id, aggregate_type, version);
+	create index get_index on events (id, aggregate_type);
 	delete from events;
 	`
 	_, err := sql.db.Exec(sqlStmt)
