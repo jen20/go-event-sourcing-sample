5f6cfd3f7068c2ed2467b34f5e9ee6cb52bd954f
hallgren <morgan.hallgren@gmail.com>
2020-12-30 16:06:49 -0500

    bbolt eventstore to implement new serializer

--- eventstore/bbolt/bbolt_test.go
+++ eventstore/bbolt/bbolt_test.go
@@ -1,9 +1,10 @@
 package bbolt_test
 
 import (
+	"encoding/json"
 	"github.com/hallgren/eventsourcing/eventstore/bbolt"
 	"github.com/hallgren/eventsourcing/eventstore/suite"
-	"github.com/hallgren/eventsourcing/serializer/json"
+	"github.com/hallgren/eventsourcing/serializer"
 	"os"
 	"testing"
 )
@@ -11,7 +12,14 @@ import (
 func TestSuite(t *testing.T) {
 	f := func() (suite.Eventstore, func(), error) {
 		dbFile := "bolt.db"
-		es := bbolt.MustOpenBBolt(dbFile, json.New())
+		ser := serializer.New(json.Marshal, json.Unmarshal)
+
+		ser.RegisterTypes(&suite.FrequentFlierAccount{},
+			func() interface{} { return &suite.FrequentFlierAccountCreated{}},
+			func() interface{} { return &suite.FlightTaken{}},
+			func() interface{} { return &suite.StatusMatched{}},
+		)
+		es := bbolt.MustOpenBBolt(dbFile, ser)
 		return es, func(){
 			es.Close()
 			os.Remove(dbFile)
